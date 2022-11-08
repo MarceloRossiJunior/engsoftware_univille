4ff6a24933356fafbc79eeed7a5992ba44dcc148
marcelojuninho7 <marcelojuninho07@gmail.com>
2022-11-08 20:44:21 -0300

    funcoes python

---
+++ segundo_semestre/algoritmos2/funcoes2/funcoes2_ex1.py
@@ -0,0 +1,6 @@
+'''
+1.	Faça uma função que receba dois vetores A e B de dez elementos inteiros, 
+por parâmetro. O procedimento deve determinar e mostrar um vetor C que 
+contenha os elementos de A e B em ordem decrescente.
+'''
+
