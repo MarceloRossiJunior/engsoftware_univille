7d9685c5ae4978965dc252ede028deb9f3600b2b
marcelojuninho7 <marcelojuninho07@gmail.com>
2022-11-13 17:54:33 -0300

    finalizado parte 1 trabalho memória

--- segundo_semestre/algoritmos2/trabalhomemoria.py
+++ segundo_semestre/algoritmos2/trabalhomemoria.py
@@ -43,91 +43,72 @@ while(opcao != 4):
     print()
     print("Escolha o algoritmo pelo numero:", end=" ")
     opcao = int(input())
-    print("Digite o tamanho da informacao:", end=" ")
-    tamanho = int(input())
-    print("Digite a letra a ser utiliada:", end=" ")
-    letra = input()
-    print()
+    if opcao != 4:  # pular o recolhimento de informações no caso de querer sair
+        print("Digite o tamanho da informacao:", end=" ")
+        tamanho = int(input())
+        print("Digite a letra a ser utiliada:", end=" ")
+        letra = input()
+        print()
 
-    if(opcao == 1):
-        #Implemente aqui a lógica da primeira escolha
-        i=0
-        while i < 100:
-            if memoria[i] == " ":
-                ini = i
-                j = ini+1
-                while j < 100:
-                    if memoria[j] != " ":
-                        fim = j
-                        break
-                    j += 1
-                espaco = fim - ini
-                if tamanho <= espaco:
-                    cont = ini
-                    while tamanho > 0:
-                        memoria[cont] = letra
-                        tamanho -= 1
-                        cont += 1
-                    break
-                if tamanho > espaco:
-                    print("Não foi possível concluir a operação. Tamanho indisponível.")
-            i += 1
-        pass
-            
-    else:
-        if (opcao == 2):
-            #Implemente aqui a lógica da melhor escolha
-            
-            pass
-        
-        else:
-            if(opcao == 3):
-                #Implemente aqui a lógica da pior escolha
-                i=0
-                while i < 100:
-                    if memoria[i] == " ":
-                        ini = i
-                        j = ini+1
-                        omaior = 0
-                        while j < 100:
-                            if memoria[j] != " ":
-                                fim = j
-                                break
-                            j += 1
-                        espaco = fim - ini
-                        if espaco > omaior:
-                            omaior = espaco
-                        else:
-                            if tamanho <= espaco:
-                                l = 0
-                                cont = ini
-                                while tamanho > 0:
-                                    memoria[cont] = letra
-                                    tamanho -= 1
-                                    cont += 1
-                                break
-                            else:
-                                print("Não é possível realizar essa operação.")
-                                print()
+        if(opcao == 1):
+            i=0
+            while i < 100:
+                if memoria[i] == " ":  # procurando um lugar vazio
+                    ini = i            # achei um lugar vazio
+                    j = ini+1          # procurando até onde vai o lugar vazio
+                    while j < 100:
+                        if memoria[j] != " ":  # encontrei até onde vai o lugar vazio
+                            fim = j
                             break
-                    i += 1
-                pass
+                        j += 1
+                    espaco = fim - ini  # espaco é igual "até onde vai" - "inicio" do lugar vazio
+                    if tamanho <= espaco:  # vendo se o tamanho cabe no espaco disponível
+                        cont = ini  #  ocupando o espaco
+                        while tamanho > 0:  # enquanto tiver dado pra ser armazenado
+                            memoria[cont] = letra  # coloca a letra na memoria
+                            tamanho -= 1  # diminui um dado
+                            cont += 1  # vai um espaco pra frente pra imprimir
+                        break
+                i += 1
+            pass
+            if tamanho > espaco:
+                print("Sem espaço. Tente um arquivo menor.")
+                print()
+            pass
 
-    for i in range(0,20):
-        print(memoria[i], end="|")
-    print()
-    for i in range(20,40):
-        print(memoria[i], end="|")
-    print()
-    for i in range(40,60):
-        print(memoria[i], end="|")
-    print()
-    for i in range(60,80):
-        print(memoria[i], end="|")
-    print()
-    for i in range(80,100):
-        print(memoria[i], end="|")
-    print()
+        else:
+            if (opcao == 2):
+                # melhor
+                pass
+                if tamanho > espaco:
+                    print("Sem espaço. Tente um arquivo menor.")
+                    print()
+                pass
+            
+            else:
+                if(opcao == 3):
+                    # pior
+                    pass
+                    if tamanho > espaco:
+                        print("Sem espaço. Tente um arquivo menor.")
+                        print()
+                    pass
+
+        for i in range(0,20):
+            print(memoria[i], end="|")
+        print()
+        for i in range(20,40):
+            print(memoria[i], end="|")
+        print()
+        for i in range(40,60):
+            print(memoria[i], end="|")
+        print()
+        for i in range(60,80):
+            print(memoria[i], end="|")
+        print()
+        for i in range(80,100):
+            print(memoria[i], end="|")
+        print()
 
 print()
 print("Compreensível. Tenha um bom dia!")
